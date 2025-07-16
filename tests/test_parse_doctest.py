4aba2e7655f3bb7abcbc986bed9522595f87fccd
Jono Yang <jyang@nexb.com>
2025-07-16 16:31:08 -0700

    Update test expectations
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- tests/test_parse_doctest.py
+++ tests/test_parse_doctest.py
@@ -29,7 +29,7 @@ r"""
     >>> tag_pattern = "<DT>?<JJ>*<NN.*>"
     >>> regexp_pattern = label_pattern_to_regex(tag_pattern)
     >>> regexp_pattern
-    '(?:<(?:DT)>)?(?:<(?:JJ)>)*(?:<(?:NN[^\\\\{\\\\}<>]*)>)'
+    '(?:<(?:DT)>)?(?:<(?:JJ)>)*(?:<(?:NN[^\\{\\}<>]*)>)'
 
 Create a Rule and parse something:
 
